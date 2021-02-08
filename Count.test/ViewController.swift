03129f26a66fc719a0376de62c4959068a599b56
Ririko Nagaishi <riry.cucumber@gmail.com>
2021-02-08 14:29:10 +0900

    カウントアプリテスト

--- Count.test/ViewController.swift
+++ Count.test/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+    var number: Int = 0
+    
+    @IBOutlet var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func plus(){
+        number = number + 1
+        label.text = String(number)
+        numberColor()
+    }
+    
+    @IBAction func minus(){
+        number = number - 1
+        label.text = String(number)
+        numberColor()
+    }
+    
+    @IBAction func clear(){
+        number = 0
+        label.text = String(number)
+        numberColor()
+    }
+    func numberColor(){
+        if number >= 10{
+            label.textColor = UIColor.red
+        }else{
+            label.textColor = UIColor.black
+        }
+        
+    }
 }
-
